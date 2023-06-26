3566cb63a68dbc989b3ba4072840861affd906fb
tagutinito <117792780+tagtagy@users.noreply.github.com>
2023-06-26 15:17:58 +0900

    p

---
+++ DADASABA/Class_EnemyCanon.cpp
@@ -0,0 +1,28 @@
+﻿#include "stdafx.h"
+#include "Class_EnemyCanon.h"
+
+//出現
+void Class_EnemyCanon::appearance() {
+
+
+
+}
+
+//ランダム出現
+void Class_EnemyCanon::random_appearance() {
+
+
+
+}
+//籠目出現
+void Class_EnemyCanon::ghost_appearance() {
+
+
+
+}
+//円出現
+void Class_EnemyCanon::circle_appearance() {
+
+
+
+}
---
+++ DADASABA/Class_EnemyCanon.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+# include "Common.h"
+#include "Class_Enemy.h"
+class Class_EnemyCanon
+{
+public:
+	//出現
+	void appearance();
+private:
+	//ランダム出現
+	void random_appearance();
+	//籠目出現
+	void ghost_appearance();
+	//円出現
+	void circle_appearance();
+private:
+
+	const double appearanceTime = 1;
+	double appearanceCount = 0;
+
+	Class_Enemy Class_Enemy[10];
+};
+
